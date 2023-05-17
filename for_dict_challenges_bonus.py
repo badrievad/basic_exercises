eb457dbea66ea6c486f75960c8e009aa7f7eae23
Алмаз <zzdl4@bk.ru>
2023-05-17 21:25:25 +0300

    for_dict_challenges_bonus done

--- for_dict_challenges_bonus.py
+++ for_dict_challenges_bonus.py
@@ -1,3 +1,6 @@
+from collections import Counter
+from itertools import groupby
+
 """
 Пожалуйста, приступайте к этой задаче после того, как вы сделали и получили ревью ко всем остальным задачам
 в этом репозитории. Она значительно сложнее.
@@ -37,6 +40,7 @@ import datetime
 import lorem
 
 
+
 def generate_chat_history():
     messages_amount = random.randint(200, 1000)
     users_ids = list(
@@ -66,5 +70,72 @@ def generate_chat_history():
     return messages
 
 
+def task1(messages):
+    users_list = [message['sent_by'] for message in messages]
+    return f'{max(users_list, key=lambda x: users_list.count(x))} - ' \
+           f'айди пользователя, который написал больше всех сообщений'
+
+
+def task2(messages):
+    answers_list = [message['reply_for'] for message in messages if message['reply_for'] is not None]
+    for i in messages:
+        if i['id'] == max(answers_list, key=lambda x: answers_list.count(x)):
+            return f'{i["sent_by"]} - айди пользователя, на сообщения которого больше всего отвечали'
+
+
+def task3(messages):
+    id_unique = {}
+    for i in messages:
+        if i['sent_by'] not in id_unique:
+            id_unique[i['sent_by']] = i['seen_by']
+        else:
+            id_unique[i['sent_by']] = id_unique[i['sent_by']] + i['seen_by']
+
+    for key, value in id_unique.items():
+        print(f'Сообщения от пользователя под айди {key}, видело {len(set(value))} уникальных пользователей')
+
+
+def task4(messages):
+    d = {
+        'утром (до 12 часов)': [],
+        'днём (12-18 часов)': [],
+        'вечером (после 18 часов)': []
+    }
+
+    for i in messages:
+        if float(i["sent_at"].strftime('%H.%M')) < 12:
+            d['утром (до 12 часов)'].append(float(i["sent_at"].strftime('%H.%M')))
+        elif 18 > float(i["sent_at"].strftime('%H.%M')) > 12:
+            d['днём (12-18 часов)'].append(float(i["sent_at"].strftime('%H.%M')))
+        else:
+            d['вечером (после 18 часов)'].append(float(i["sent_at"].strftime('%H.%M')))
+
+    return f'В чате больше всего сообщений: {max(d, key=lambda x: len(d[x]))}'
+
+def task5(messages):
+    result = []
+
+    for k, g in groupby([i['reply_for'] for i in messages]):
+        length = len(list(g))
+        result.append((k, length))
+
+    lst = [(key, value) for key, value in sorted(result, key=lambda x: x[1], reverse=True) if
+           key is not None and value > 1]
+
+    if len(lst) > 0:
+        for key, value in lst:
+            print(f'Сообщение под айди: "{key}" стало началом для самых длинных тредов (цепочек ответов), а именно '
+                  f'{value}.')
+    else:
+        print('Максимальная длина тредов (цепочек ответов) составляет 1.')
+
 if __name__ == "__main__":
-    print(generate_chat_history())
+    print(task1(generate_chat_history()))
+    print('_' * 75)
+    print(task2(generate_chat_history()))
+    print('_' * 75)
+    task3(generate_chat_history())
+    print('_' * 75)
+    print(task4(generate_chat_history()))
+    print('_' * 75)
+    task5(generate_chat_history())
